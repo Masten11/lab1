0bce2996a3ac99b91e84f27ba5d7762ea7aefb60
Masten11 <112028223+Masten11@users.noreply.github.com>
2025-03-07 11:27:25 +0100

    Update CarTransport.java

--- CarTransport.java
+++ CarTransport.java
@@ -2,21 +2,19 @@ import java.awt.*;
 
 
 // Main CarTransport class using composition
-public class CarTransport extends Car implements RampInterface{
-    private final Ramp ramp;
+public class CarTransport extends Truck{
     private static final double MAX_CAR_LENGTH = 5.5;
     private static final double MAX_CAR_WIDTH = 2.0;
     private final CarLoader<Car> carLoader;
 
     public CarTransport(int maxLoad) {
         super(2, 80, Color.gray, "TheLongtrader", 10, 2.5); // Initialize Car's attributes
-        this.ramp = new Ramp();
         this.carLoader = new CarLoader<>(maxLoad);
     }
 
 
     public void loadCar(Car car) {
-        if (!ramp.isRampDown()) {
+        if (!getRamp().isRampDown()) {
             throw new IllegalStateException("Ramp must be lowered to load a car!");
         }
         if (car instanceof CarTransport) {
@@ -30,7 +28,7 @@ public class CarTransport extends Car implements RampInterface{
     }
 
     public void unloadCar() {
-        if (!ramp.isRampDown()) {
+        if (!getRamp().isRampDown()) {
             throw new IllegalStateException("Ramp must be lowered to unload a car");
         }
         carLoader.unloadCar(getX(), getY());
@@ -38,7 +36,7 @@ public class CarTransport extends Car implements RampInterface{
 
     @Override
     public void move() {
-        if (ramp.isRampDown()) {
+        if (getRamp().isRampDown()) {
             throw new IllegalArgumentException("Ramp cannot be down when moving");
         }
 
@@ -51,32 +49,4 @@ public class CarTransport extends Car implements RampInterface{
         }
     }
 
-    @Override
-    protected double speedFactor() {
-        return getEnginePower() * 0.01;
-    }
-
-    @Override
-    public void raise() {
-        if (getCurrentSpeed() > 0) {
-            throw new IllegalArgumentException("The truck cannot be moving!");
-        }
-        ramp.raise();
-    }
-
-    @Override
-    public void lower() {
-        if (getCurrentSpeed() > 0) {
-            throw new IllegalArgumentException("The truck cannot be moving!");
-        }
-        ramp.lower();
-    }
-
-    @Override
-    public void gas(double amount) {
-        if (ramp.isRampDown()) {
-            throw new IllegalArgumentException("Ramp cannot be down when moving");
-        }
-        super.gas(amount);
-    }
 }
