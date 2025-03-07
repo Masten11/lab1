92208f2b6e5e408e741b0acd48bf16055451dfca
Masten11 <112028223+Masten11@users.noreply.github.com>
2025-03-07 11:25:25 +0100

    Update Scania.java

--- Scania.java
+++ Scania.java
@@ -1,45 +1,26 @@
 import java.awt.*;
 
-public class Scania extends Car implements RampInterface{
-    private final Ramp ramp;
+public class Scania extends Truck{
 
     // Constructor to initialize Scania attributes
     public Scania() {
         super(2, 90, Color.white, "Scania", 7.0, 2.2); // Initialize Car's attributes
-        this.ramp = new Ramp();
-    }
-
-    @Override
-    protected double speedFactor() {
-        return getEnginePower() * 0.01;
     }
 
 
-    //ifall ingen vinkel anges sätts vinkeln till min
     @Override
-    public void raise() {
-        if (getCurrentSpeed() > 0) {
-            throw new IllegalArgumentException("The truck cannot be moving");
+    public void move() {
+        if (getRamp().getAngle() > 0) {
+            throw new IllegalArgumentException("Angle cannot be greater than 0");
         }
-        ramp.raise(70);
+        super.move();
     }
-
 
     public void raise(double amount) {
         if (getCurrentSpeed() > 0) {
             throw new IllegalArgumentException("The truck cannot be moving");
         }
-        ramp.raise(amount);
-    }
-
-
-    //ifall ingen vinkel anges sätts vinkeln till max
-    @Override
-    public void lower() {
-        if (getCurrentSpeed() > 0) {
-            throw new IllegalArgumentException("The truck cannot be moving");
-        }
-        ramp.lower(70);
+        getRamp().raise(amount);
     }
 
     // Lower the ramp by a specific amount
@@ -47,30 +28,8 @@ public class Scania extends Car implements RampInterface{
         if (getCurrentSpeed() > 0) {
             throw new IllegalArgumentException("The truck cannot be moving");
         }
-        ramp.lower(amount);
+        getRamp().lower(amount);
     }
 
-    @Override
-    public void move() {
-        if (ramp.getAngle() < 70) {
-            throw new IllegalArgumentException("Angle cannot be greater than 0");
-        }
-        super.move();
-    }
-
-    @Override
-    protected void startEngine() {
-        if (ramp.getAngle()==0) {
-            super.startEngine();
-        }
-    }
-
-    @Override
-    public void gas(double amount){
-        if (ramp.getAngle() < 70) {
-            throw new IllegalArgumentException("Angle cannot be greater than 0");
-        }
-        super.gas(amount);
-    }
 
 }
